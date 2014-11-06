82f6811f4c9cbcbc4c065ace1b34014f66b0e221
rhall12 <rth45@cornell.edu>
2014-11-05 23:20:47 -0500

    Broken layer button

--- land_server/land_server/urls.py
+++ land_server/land_server/urls.py
@@ -2,11 +2,15 @@ from django.conf.urls import patterns, include, url
 
 from views import MapView
 from django.contrib import admin
+
+import views
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', MapView.as_view(), name='map_display'),
+    #url(r'^', views.error, name='error'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
--- land_server/land_server/views.py
+++ land_server/land_server/views.py
@@ -1,14 +1,30 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.views import generic
 
 import ee
 
+def error(request):
+	print('Error in server')
+	return Http404
+
 class MapView(generic.View):
+	def extract_layer(layer):
+		print ('in extract')
+		data = { 'This layer has been extracted':layer }
+		return data
+
 	def post(self, request):
-		pass
+		print(request.POST)
+		layer = request.POST['layer_title'];
+
+		print(layer)
+		data = extract_layer(layer)
+		print("Got data")
+
+		return HttpResponse(json.dumps(data));
 
 	def get(self, request):
 		return render(request, 'land_server/map_display.html')
 
-	def extract_layer():
-		pass
+	
